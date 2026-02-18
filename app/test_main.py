4a32f2f116e470307de4de50472b99ee313a7798
Jerzy Joniec <j.joniec5@gmail.com>
2026-02-18 11:36:18 +0100

    „final”

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,20 @@
-# write your code here
+from unittest.mock import patch
+from app.main import cryptocurrency_action
+
+
+def test_recommends_buy_when_prediction_over_5_percent_higher() -> None:
+    with patch("app.main.get_exchange_rate_prediction") as mock_prediction:
+        mock_prediction.return_value = 1.6
+        assert cryptocurrency_action(1) == "Buy more cryptocurrency"
+
+
+def test_recommends_buy_when_prediction_over_5_percent_lower() -> None:
+    with patch("app.main.get_exchange_rate_prediction") as mock_prediction:
+        mock_prediction.return_value = 1
+        assert cryptocurrency_action(1.6) == "Sell all your cryptocurrency"
+
+
+def test_recommends_buy_when_prediction_is_the_same() -> None:
+    with patch("app.main.get_exchange_rate_prediction") as mock_prediction:
+        mock_prediction.return_value = 1
+        assert cryptocurrency_action(1) == "Do nothing"
