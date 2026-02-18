9ca06baff3501824f3317083256523668242062d
Jerzy Joniec <j.joniec5@gmail.com>
2026-02-18 12:08:34 +0100

    „final”

--- app/test_main.py
+++ app/test_main.py
@@ -2,31 +2,31 @@ from unittest.mock import patch
 from app.main import cryptocurrency_action
 
 
-def test_recommends_buy_when_prediction_over_5_percent_higher() -> None:
+def test_buy_over_5_percent():
     with patch("app.main.get_exchange_rate_prediction") as mock_prediction:
         mock_prediction.return_value = 1.06
         assert cryptocurrency_action(1) == "Buy more cryptocurrency"
 
 
-def test_recommends_sell_when_prediction_over_5_percent_lower() -> None:
+def test_sell_over_5_percent():
     with patch("app.main.get_exchange_rate_prediction") as mock_prediction:
-        mock_prediction.return_value = 1
-        assert cryptocurrency_action(1.06) == "Sell all your cryptocurrency"
+        mock_prediction.return_value = 0.94
+        assert cryptocurrency_action(1) == "Sell all your cryptocurrency"
 
 
-def test_recommends_do_nothing_when_prediction_is_the_same() -> None:
+def test_do_nothing_same_rate():
     with patch("app.main.get_exchange_rate_prediction") as mock_prediction:
         mock_prediction.return_value = 1
         assert cryptocurrency_action(1) == "Do nothing"
 
 
-def test_recommends_buy_when_ratio_exactly_105_percent() -> None:
+def test_do_nothing_at_plus_5_percent_boundary():
     with patch("app.main.get_exchange_rate_prediction") as mock_prediction:
-        mock_prediction.return_value = 1
-        assert cryptocurrency_action(1.05) == "Buy more cryptocurrency"
+        mock_prediction.return_value = 1.05
+        assert cryptocurrency_action(1) == "Do nothing"
 
 
-def test_recommends_sell_when_ratio_exactly_95_percent() -> None:
+def test_do_nothing_at_minus_5_percent_boundary():
     with patch("app.main.get_exchange_rate_prediction") as mock_prediction:
-        mock_prediction.return_value = 1
-        assert cryptocurrency_action(0.95) == "Sell all your cryptocurrency"
+        mock_prediction.return_value = 0.95
+        assert cryptocurrency_action(1) == "Do nothing"
