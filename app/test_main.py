9d47db2a9c8d8da5350e1dba4840a6ab1413f751
Jerzy Joniec <j.joniec5@gmail.com>
2026-02-18 11:40:54 +0100

    „final”

--- app/test_main.py
+++ app/test_main.py
@@ -4,14 +4,14 @@ from app.main import cryptocurrency_action
 
 def test_recommends_buy_when_prediction_over_5_percent_higher() -> None:
     with patch("app.main.get_exchange_rate_prediction") as mock_prediction:
-        mock_prediction.return_value = 1.6
+        mock_prediction.return_value = 1.06
         assert cryptocurrency_action(1) == "Buy more cryptocurrency"
 
 
 def test_recommends_buy_when_prediction_over_5_percent_lower() -> None:
     with patch("app.main.get_exchange_rate_prediction") as mock_prediction:
         mock_prediction.return_value = 1
-        assert cryptocurrency_action(1.6) == "Sell all your cryptocurrency"
+        assert cryptocurrency_action(1.06) == "Sell all your cryptocurrency"
 
 
 def test_recommends_buy_when_prediction_is_the_same() -> None:
